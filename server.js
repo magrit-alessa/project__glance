767b8584d0c4e3a44cf2610540a13ac5ddb0c7c0
magrit-alessa <magrit.alessa@gmail.com>
2018-02-28 23:00:34 +0200

    for heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+const PORT = process.evn.PORT || 3000;
+app.use(function(req, res, next){
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+
+	}else{
+      res.redirect('http://' + req.hostname + req.url);
+	}
+});
+
+app.use(express.static('public'));
+app.listen(PORT, function(){
+	console.log('Server is up on port ' + PORT);
+});
